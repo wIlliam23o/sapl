fb3d5ea5d999153ddba092217e9cdb927e43c9d5
Leandro Roberto <leandroroberto.br@gmail.com>
2018-08-06 14:10:20 -0300

    fragmenta app api para pastas por app do projeto

--- sapl/api/base/forms.py
+++ sapl/api/base/forms.py
@@ -1,93 +1,14 @@
-import logging
-
 from django.db.models import Q
-from django.forms.fields import CharField, MultiValueField
-from django.forms.widgets import MultiWidget, TextInput
 from django.utils import timezone
-from django_filters.filters import MethodFilter, ModelChoiceFilter, DateFilter
+from django.utils.translation import ugettext_lazy as _
+from django_filters.filters import DateFilter, MethodFilter, ModelChoiceFilter
 from rest_framework import serializers
-from rest_framework.compat import django_filters
 from rest_framework.filters import FilterSet
 
-from sapl.base.models import TipoAutor, Autor
+from sapl.api.forms import SaplGenericRelationSearchFilterSet,\
+    SearchForFieldFilter
+from sapl.base.models import Autor, TipoAutor
 from sapl.parlamentares.models import Legislatura
-from sapl.utils import generic_relations_for_model
-
-
-class SaplGenericRelationSearchFilterSet(FilterSet):
-    q = MethodFilter()
-
-    def filter_q(self, queryset, value):
-
-        query = value.split(' ')
-        if query:
-            q = Q()
-            for qtext in query:
-                if not qtext:
-                    continue
-                q_fs = Q(nome__icontains=qtext)
-
-                order_by = []
-
-                for gr in generic_relations_for_model(self._meta.model):
-                    sgr = gr[1]
-                    for item in sgr:
-                        if item.related_model != self._meta.model:
-
-                            continue
-                        flag_order_by = True
-                        for field in item.fields_search:
-                            if flag_order_by:
-                                flag_order_by = False
-                                order_by.append('%s__%s' % (
-                                    item.related_query_name(),
-                                    field[0])
-                                )
-                            # if len(field) == 3 and field[2](qtext) is not
-                            # None:
-                            q_fs = q_fs | Q(**{'%s__%s%s' % (
-                                item.related_query_name(),
-                                field[0],
-                                field[1]): qtext if len(field) == 2
-                                else field[2](qtext)})
-
-                q = q & q_fs
-
-            if q:
-                queryset = queryset.filter(q).order_by(*order_by)
-
-        return queryset
-
-
-class SearchForFieldWidget(MultiWidget):
-
-    def decompress(self, value):
-        if value is None:
-            return [None, None]
-        return value
-
-    def __init__(self, attrs=None):
-        widgets = (TextInput, TextInput)
-        MultiWidget.__init__(self, widgets, attrs)
-
-
-class SearchForFieldField(MultiValueField):
-    widget = SearchForFieldWidget
-
-    def __init__(self, *args, **kwargs):
-        fields = (
-            CharField(),
-            CharField())
-        super(SearchForFieldField, self).__init__(fields, *args, **kwargs)
-
-    def compress(self, parameters):
-        if parameters:
-            return parameters
-        return None
-
-
-class SearchForFieldFilter(django_filters.filters.MethodFilter):
-    field_class = SearchForFieldField
 
 
 class AutorChoiceFilterSet(SaplGenericRelationSearchFilterSet):
@@ -125,9 +46,8 @@ class AutorSearchForFieldFilterSet(AutorChoiceFilterSet):
 
 
 class AutoresPossiveisFilterSet(FilterSet):
-    logger = logging.getLogger(__name__)
     data_relativa = DateFilter(method='filter_data_relativa')
-    tipo = MethodFilter()
+    tipo = MethodFilter(required=True)
 
     class Meta:
         model = Autor
@@ -137,13 +57,9 @@ class AutoresPossiveisFilterSet(FilterSet):
         return queryset
 
     def filter_tipo(self, queryset, value):
-
         try:
-            self.logger.debug(
-                "Tentando obter TipoAutor correspondente à pk {}.".format(value))
             tipo = TipoAutor.objects.get(pk=value)
         except:
-            self.logger.error("TipoAutor(pk={}) inexistente.".format(value))
             raise serializers.ValidationError(_('Tipo de Autor inexistente.'))
 
         qs = queryset.filter(tipo=tipo)
@@ -160,11 +76,9 @@ class AutoresPossiveisFilterSet(FilterSet):
         tipo = self.form.cleaned_data['tipo'] \
             if 'tipo' in self.form.cleaned_data else None
 
-        if not tipo and not data_relativa:
+        if not tipo:
             return qs
-
-        if tipo:
-            # não precisa de try except, já foi validado em filter_tipo
+        else:
             tipo = TipoAutor.objects.get(pk=tipo)
             if not tipo.content_type:
                 return qs
