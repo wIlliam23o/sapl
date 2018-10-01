74bdcf061c149029b09e5c51fdfb6acac48f23e8
Leandro Roberto <leandroroberto.br@gmail.com>
2018-10-01 10:47:10 -0300

    add tipo de interpretação das datas
    tipo_update = 1
        data_min e data_max -> relativo sessões alteradas nesse período
    tipo_update = 2
        data_min e data_max -> relativo a sessões com data_inicio constantes
    nesse intervalo

--- sapl/api/sessao/views.py
+++ sapl/api/sessao/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.db.models import Q
 from django.utils.timezone import utc
 from rest_framework.filters import DjangoFilterBackend
 from rest_framework.permissions import AllowAny
@@ -29,21 +30,47 @@ class SessaoPlenariaViewSet(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         qs = super().get_queryset()
+        opts = SessaoPlenaria._meta
 
-        data = self.request.query_params.get('data', None)
-        if data:
-            data = datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%f')
-            data = data.replace(tzinfo=utc)
-            opts = SessaoPlenaria._meta
-            vs = Version.objects.filter(
-                revision__date_created__gte=data,
-                content_type__app_label=opts.app_label,
-                content_type__model=opts.model_name
-            ).order_by(
-                '-revision__date_created', '-object_id'
-            ).values_list('object_id', flat=True)
-            vs = set(vs)
+        data_min = self.request.query_params.get('data_min', None)
+        data_max = self.request.query_params.get('data_max', None)
+        tipo_update = self.request.query_params.get('tipo_update', '1')
 
-            qs = qs.filter(id__in=vs)
+        if data_min:
+            data_min = datetime.strptime(data_min, '%Y-%m-%dT%H:%M:%S.%f')
+            data_min = data_min.replace(tzinfo=utc)
+        if data_max:
+            data_max = datetime.strptime(data_max, '%Y-%m-%dT%H:%M:%S.%f')
+            data_max = data_max.replace(tzinfo=utc)
+
+        if data_min or data_max:
+            if tipo_update == '1':
+                q = Q(content_type__app_label=opts.app_label,
+                      content_type__model=opts.model_name)
+
+                if data_min:
+                    q &= Q(revision__date_created__gte=data_min)
+
+                if data_max:
+                    q &= Q(revision__date_created__lte=data_max)
+
+                vs = Version.objects.filter(q).order_by(
+                    '-revision__date_created', '-object_id'
+                ).values_list('object_id', flat=True)
+                vs = set(vs)
+                qs = qs.filter(id__in=vs)
+            elif tipo_update == '2':
+                """
+                    apesar de a data estar ser datetime e o campo data_inicio
+                    ser DateField, devido a este fato, as partes de um dia
+                    é descartada no filtro. 
+                """
+                q = Q()
+                if data_min:
+                    q &= Q(data_inicio__gte=data_min)
+                if data_max:
+                    q &= Q(data_inicio__lte=data_max)
+
+                qs = qs.filter(q)
 
         return qs
