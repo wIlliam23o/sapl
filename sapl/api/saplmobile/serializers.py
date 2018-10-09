e45894ca88b3f470b78cec8c85606bac5f26d1fa
Leandro Roberto <leandroroberto.br@gmail.com>
2018-10-09 13:22:02 -0300

    retorna cropping url relativo das imagens dos parlamentares

--- sapl/api/saplmobile/serializers.py
+++ sapl/api/saplmobile/serializers.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import os
 
 from django.utils.timezone import make_aware
+from image_cropping.utils import get_backend
 from rest_framework import serializers
 
-from sapl.materia.models import MateriaLegislativa
+from sapl.base.models import Autor
+from sapl.materia.models import MateriaLegislativa, Autoria
 from sapl.sessao.models import SessaoPlenaria, OrdemDia, ExpedienteMateria,\
     RegistroVotacao
 
@@ -27,23 +29,49 @@ class SessaoPlenariaSerializer(serializers.ModelSerializer):
                   'numero',)
 
 
+class AutorSerializer(serializers.ModelSerializer):
+    fotografia = serializers.SerializerMethodField()
+    file_date_updated = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Autor
+        fields = ('id', 'nome', 'fotografia', 'file_date_updated')
+
+    def get_fotografia(self, obj):
+        if obj.autor_related:
+            if hasattr(obj.autor_related, 'fotografia'):
+                thumbnail_url = get_backend().get_thumbnail_url(
+                    obj.autor_related.fotografia,
+                    {
+                        'size': (128, 128),
+                        'box':  obj.autor_related.cropping,
+                        'crop': True,
+                        'detail': True,
+                    }
+                )
+
+                return thumbnail_url
+        return ''
+
+    def get_file_date_updated(self, obj):
+        if obj.autor_related:
+            if hasattr(obj.autor_related, 'fotografia'):
+                file = obj.autor_related.fotografia
+                try:
+                    lastmodified = os.stat(file.path).st_mtime
+                    return make_aware(
+                        datetime.utcfromtimestamp(lastmodified)
+                    ).isoformat(timespec='milliseconds')[:-6]
+                except:
+                    return None
+        return None
+
+
 class MateriaLegislativaSerializerMixin(serializers.ModelSerializer):
     tipo = serializers.StringRelatedField(many=False)
     tipo_sigla = serializers.SerializerMethodField()
     file_date_updated = serializers.SerializerMethodField()
-
-    def get_tipo_sigla(self, obj):
-        return obj.tipo.sigla
-
-    def get_file_date_updated(self, obj):
-        file = obj.texto_original
-        try:
-            lastmodified = os.stat(file.path).st_mtime
-            return make_aware(
-                datetime.utcfromtimestamp(lastmodified)
-            ).isoformat(timespec='milliseconds')[:-6]
-        except Exception as e:
-            return None
+    autores = AutorSerializer(many=True)
 
     class Meta:
         model = MateriaLegislativa
@@ -59,6 +87,19 @@ class MateriaLegislativaSerializerMixin(serializers.ModelSerializer):
                   'autores',
                   'file_date_updated'
                   )
+
+    def get_tipo_sigla(self, obj):
+        return obj.tipo.sigla
+
+    def get_file_date_updated(self, obj):
+        file = obj.texto_original
+        try:
+            lastmodified = os.stat(file.path).st_mtime
+            return make_aware(
+                datetime.utcfromtimestamp(lastmodified)
+            ).isoformat(timespec='milliseconds')[:-6]
+        except Exception as e:
+            return None
 
 
 class MateriaLegislativaSerializer(MateriaLegislativaSerializerMixin):
