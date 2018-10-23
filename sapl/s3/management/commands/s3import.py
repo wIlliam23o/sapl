7a985ca7313e4d1cc1c15d5181c9a4f04f0f1126
Leandro Roberto <leandroroberto.br@gmail.com>
2018-10-23 10:33:23 -0300

    teste legislatura

--- sapl/s3/management/commands/s3import.py
+++ sapl/s3/management/commands/s3import.py
@@ -1,7 +1,24 @@
 from django.core.management.base import BaseCommand
 
+from sapl.parlamentares.models import Legislatura
+from sapl.s3.models import S3Legislatura
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("s3 import run")
+
+        legislaturas = S3Legislatura.objects.all()
+        for leg in legislaturas:
+            if leg.ind_excluido:
+                continue
+
+            legis = Legislatura()
+            legis.id = leg.num_legislatura
+            legis.numero = leg.num_legislatura
+            legis.data_eleicao = leg.dat_eleicao
+            legis.data_inicio = leg.dat_inicio
+            legis.data_fim = leg.dat_fim
+            legis.save()
+
+            print(leg.num_legislatura)
